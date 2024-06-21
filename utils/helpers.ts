c431f693b9ffdbb800d5d67ad26a99de11f3e1bc
iNikolas <realkolos1@gmail.com>
2024-06-21 17:21:32 +0300

    url protocol

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -4,16 +4,24 @@ export async function isValidUrl(input?: string): Promise<boolean> {
   }
 
   try {
-    new URL(input);
+    const url = new URL(input);
 
-    return await isUrlExists(input);
+    if (url.protocol !== "http:" && url.protocol !== "https:") {
+      return false;
+    }
+
+    return await isUrlExists(url.toString());
   } catch (error) {
     return false;
   }
 }
 
 export async function isUrlExists(url: string): Promise<boolean> {
-  const response = await fetch(url, { method: "HEAD" });
+  try {
+    const response = await fetch(url, { method: "HEAD" });
 
-  return response.ok;
+    return response.ok;
+  } catch (error) {
+    return false;
+  }
 }
